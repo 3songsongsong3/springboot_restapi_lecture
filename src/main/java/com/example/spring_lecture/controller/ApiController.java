780722f9f0368d086ae4175cf8734dfdb06f9f6c
송 유진 <tlahsh12@naver.com>
2023-10-31 23:15:39 +0900

    RESPONSE 반환

--- src/main/java/com/example/spring_lecture/controller/ApiController.java
+++ src/main/java/com/example/spring_lecture/controller/ApiController.java
@@ -1,8 +1,9 @@
 package com.example.spring_lecture.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.spring_lecture.dto.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController // 해당 클래스는 REST API 처리하는 Controller
 @RequestMapping("/api")
@@ -12,4 +13,25 @@ public class ApiController {
     public String hello() {
         return "Hello spring boot";
     }
+
+    // TEXT
+    @GetMapping("/text")
+    public String text(@RequestParam String account) {
+        return account;
+    }
+
+    // JSON
+    @PostMapping("/json")
+    public User json(@RequestBody User user) {
+        return user;
+    }
+
+    // 응답에 대한 커스터마이징이 필요할 때, ResponseEntity 사용 가능
+    @PutMapping("/put")
+    public ResponseEntity<User> put(@RequestBody User user) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
 }
+
+
